8b0ff91ea6508fc8958b94716d81e649d2140766
JUB0T <jforeverything2007@gmail.com>
2022-05-08 22:26:57 +0200

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
 require("dotenv").config();
 
-const Console = require("./handler/console");
-const view = require("./handler/view");
-const path = require("path");
+const Console = require("../handler/console");
+const view = require("../handler/view");
 const express = require("express");
+const path = require("path");
 const app = express();
 
 app.set("view engine", "html");
 app.engine("html", require("ejs").renderFile);
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/js", require("./routes/js"));
-app.use("/api", require("./routes/api"));
-app.use("/css", require("./routes/css"));
-app.use("/components", require("./routes/components"));
+app.use("/js", require("../routes/js"));
+app.use("/api", require("../routes/api"));
+app.use("/css", require("../routes/css"));
+app.use("/components", require("../routes/components"));
 
 app.get("/", function (req, res) {
   res.sendFile(view(`index`));
